48f7d6a341ebf33c42dc4f16fadb521e9c893980
김시인 <ksi8548@gmail.com>
2022-06-21 00:09:49 +0900

    다형성 복습
    결과 : 포인터가 업캐스팅 되어있어도 파생 클래스의 오버라이딩된 함수가 잘 불러와진다.

--- TestProject/TestProject/소스.cpp
+++ TestProject/TestProject/소스.cpp
@@ -9,47 +9,32 @@
 
 using namespace std;
 
-random_device rd;
-default_random_engine dre{ rd() };
-uniform_int_distribution<int> uid;
+class Animal {
+public:
+	virtual void Print() {
+		cout << "나는 동물이야" << endl;
+	}
+};
+
+class Dog : public Animal {
+public:
+	virtual void Print() {
+		Animal::Print();
+		cout << "나는 개야" << endl;
+	}
+};
+
+class Retriever : public Dog {
+public:
+	virtual void Print() {
+		Dog::Print();
+		cout << "나는 리트리버야" << endl;
+	}
+};
 
 int main() 
 {
-	//저장하기
-	//ofstream out("LoadData", ios::binary);
-
-	//for (int i = 0; i < 10'000'000; ++i) {	//천만개
-	//	int temp = uid(dre);
-	//	out.write((const char*)&temp, sizeof(temp));
-	//}
-
-	vector<int> dumy(10'000'000);
-	for (int i = 0; i < 10'000'000; ++i) {	//천만개
-		dumy[i] = uid(dre);
-	}
-
-	//읽어오기
-	auto start1 = chrono::steady_clock::now();
-
-	ifstream in("LoadData", ios::binary);
-	vector<int> v(10'000'000);
-	in.read((char*)v.data(), sizeof(int) * 10'000'000);
-	//cout << *v.rbegin() << endl;
-
-	auto stop1 = chrono::steady_clock::now();
-	auto et1 = chrono::duration_cast<chrono::microseconds>(stop1 - start1).count();
-	cout << format("binary file 에서 vector로 읽어오기 - {0:8} us", et1) << endl;
-
-	//복사하기
-	auto start2 = chrono::steady_clock::now();
-
-	vector<int> v2(10'000'000);
-	ranges::copy(dumy, v2.begin());
-	//cout << *v2.rbegin() << endl;
-
-	auto stop2 = chrono::steady_clock::now();
-	auto et2 = chrono::duration_cast<chrono::microseconds>(stop2 - start2).count();
-	cout << format("vector에서 vector로 copy하기 	   - {0:8} us", et2) << endl;
-
-	//cout << "1";
+	Animal* retriever = new Retriever();
+	retriever->Print();
+	delete retriever;
 }
