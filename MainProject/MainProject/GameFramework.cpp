5102a9bb1fff48222dbcbacaedeb7d663395e017
김시인 <ksi8548@gmail.com>
2022-06-21 16:39:53 +0900

    GameFramework 기본 생성자와 소멸자 추가
    GameFramework의 기본 생성자에서 멤버변수들을 초기화 시켜준다. 기본적으로 포이터는 전부 NULL값을 가지도록 초기화 시켰다.

--- MainProject/MainProject/GameFramework.cpp
+++ MainProject/MainProject/GameFramework.cpp
@@ -1,4 +1,56 @@
 #include "stdafx.h"
 #include "GameFramework.h"
 
+// 기본 생성자
+GameFramework::GameFramework() 
+{
+	// 윈도우
+	m_hInstance = NULL;
+	m_hWnd = NULL;
+
+	// 클라이언트 해상도
+	m_windowClientWidth = FRAME_BUFFER_WIDTH;
+	m_windowClientHeight = FRAME_BUFFER_HEIGHT;
+
+	// 팩토리, 스왑체인, 디바이스
+	m_pDxgiFactory = NULL;
+	m_pDxgiSwapChain = NULL;
+	m_pD3dDevice = NULL;
+
+	// MSAA 다중 샘플링
+	m_msaa4xEnable = false;
+	m_msaa4xQualityLevels = 0;
+
+	// 현재 스왑체인의 후면 버퍼의 인덱스
+	m_swapChainBufferCurrentIndex = 0;
+
+	// 렌더 타겟 초기화
+	for (int i = 0; i < m_nSwapChainBuffers; ++i)
+		m_ppD3dRenderTargetBuffers[i] = NULL;
+	m_pd3dRtvDescriptorHeap = NULL;
+	m_rtvDescriptorIncrementSize = 0;
+
+	// 깊이-스텐실 초기화
+	m_pD3dDepthStencilBuffer = NULL;
+	m_pD3dDsvDescriptorHeap = NULL;
+	m_dsvDescriptorIncrementSize = 0;
+
+	// 명령 큐, 할당자, 리스트 초기화
+	m_pD3dCommandQueue = NULL;
+	m_pD3dCommandAllocator = NULL;
+	m_pD3dCommandList = NULL;
+
+	//펜스 초기화
+	m_pD3dFence = NULL;
+	m_fenceEvent = NULL;
+	for (int i = 0; i < m_nSwapChainBuffers; ++i)
+		m_fenceValues[i] = NULL;
+
+}
+// 소멸자
+GameFramework::~GameFramework() 
+{
+
+}
+
 
--- MainProject/MainProject/GameFramework.h
+++ MainProject/MainProject/GameFramework.h
@@ -4,6 +4,7 @@ private:
 	HINSTANCE m_hInstance;
 	HWND m_hWnd;
 
+	//클라이언트 해상도
 	int m_windowClientWidth;
 	int m_windowClientHeight;
 
@@ -12,8 +13,8 @@ private:
 	ID3D12Device* m_pD3dDevice;	///Direct3D 디바이스 인터페이스에 대한 포인터이다. 주로 리소스를 생성하기 위하여 필요하다.
 
 	//MSAA 다중 샘플링을 활성화하고 다중 샘플링 레벨을 설정한다
-	bool m_msaa4xEnable = false;
-	UINT m_msaa4xQualityLevels = 0;
+	bool m_msaa4xEnable;
+	UINT m_msaa4xQualityLevels;
 
 	static const UINT m_nSwapChainBuffers = 2;	//스왑 체인의 후면 버퍼의 개수이다.
 	UINT m_swapChainBufferCurrentIndex; //현재 스왑 체인의 후면 버퍼 인덱스이다.
@@ -24,7 +25,7 @@ private:
 	UINT m_rtvDescriptorIncrementSize;	//렌더 타겟 서술자 원소의 크기이다.
 	
 	//깊이-스텐실 버퍼, 서술자 힙 인터페이스 포인터, 깊이-스텐실 서술자 원소의 크기이다.
-	ID3D12Resource* m_pD3dDepthStencilBuffer;
+	ID3D12Resource* m_pD3dDepthStencilBuffer;	// 깊이-스텐실 버퍼는 렌더타겟버퍼와 다르게 1개만 있으면 된다.
 	ID3D12DescriptorHeap* m_pD3dDsvDescriptorHeap;
 	UINT m_dsvDescriptorIncrementSize;
 
@@ -33,10 +34,17 @@ private:
 	ID3D12CommandAllocator* m_pD3dCommandAllocator;
 	ID3D12GraphicsCommandList* m_pD3dCommandList;
 	
-	//펜스 인터페이스 포인터, 펜스의 값, 이벤트 핸들이다. 
+	//펜스 인터페이스 포인터, 이벤트 핸들, 펜스의 값 이다. 
 	ID3D12Fence* m_pD3dFence;
-	UINT64 m_fenceValue;
 	HANDLE m_fenceEvent;
+	UINT64 m_fenceValues[m_nSwapChainBuffers];
+	
+
+public:
+	// 생성자 및 소멸자
+	GameFramework();
+	~GameFramework();
 
 };
+
 
