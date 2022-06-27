5d61db94f3a2a9644f22b0476a86d5fb1922dc75
김시인 <ksi8548@gmail.com>
2022-06-27 18:55:35 +0900

    GameObject 오류 수정
    생성자에서 m_pParent = nullptr; 와 같이 초기화 해야하는데 GameObject* m_pSibling = nullptr; 와 같이 변수를 생성하는 걸로 잘못 적었다.
    + 추가 주석

--- MainProject/MainProject/GameObject.cpp
+++ MainProject/MainProject/GameObject.cpp
@@ -1,20 +1,20 @@
 #include "stdafx.h"
 #include "GameObject.h"
 
+// 생성자, 소멸자
 GameObject::GameObject() 
 {
-	m_nReferenced = 0;	// 이 객체를 참조하고 있는 수
+	m_nReferenced = 0;	// 이 객체를 참조하고 있는 수 (0이 될때 delete를 이용하여 제거 되도록 한다.)
+	m_destroyed = false;;	// 해당 객체가 파괴됬는지 확인 (true일 경우 참조하는 쪽에서 참조를 끊는다. => 끊어지다 보면 m_nReferenced가 0이 되어 메모리가 반환될 것이다. + true가 될때 내가 참조하고 있는 포인터들을 전부 끊는다.)
 
 	m_name = "이름없음";	// 객체의 이름
-
-	m_destroyed = false;;	// 해당 객체가 파괴됬는지 확인
 
 	Matrix4x4::Identity(m_worldTransform);	// 모델->월드 좌표로 바꾸기 위한 행렬
 	Matrix4x4::Identity(m_eachTransform);	// 부모 좌표 기준으로 자신을 변환하는 행렬
 
-	GameObject* m_pParent = nullptr;	// 부모에 대한 포인터, nullptr일 경우 부모가 없다.
-	GameObject* m_pChild = nullptr;		// 자식에 대한 포인터
-	GameObject* m_pSibling = nullptr;	// 형제에 대한 포인터
+	m_pParent = nullptr;	// 부모에 대한 포인터, nullptr일 경우 부모가 없다.
+	m_pChild = nullptr;		// 자식에 대한 포인터
+	m_pSibling = nullptr;	// 형제에 대한 포인터
 
 	m_OOBB = BoundingOrientedBox();
 	m_isOOBBCover = false;
@@ -22,4 +22,7 @@ GameObject::GameObject()
 	// 매쉬 포인터 초기화
 	// 메테리얼 포인터들 초기화
 
+}
+GameObject::~GameObject() {
+	
 }
--- MainProject/MainProject/GameObject.h
+++ MainProject/MainProject/GameObject.h
@@ -2,11 +2,10 @@
 
 class GameObject {
 protected:
-	int m_nReferenced;	// 이 객체를 참조하고 있는 수
+	int m_nReferenced;	// 이 객체를 참조하고 있는 수 (0이 될때 delete를 이용하여 제거 되도록 한다.)
+	bool m_destroyed;	// 해당 객체가 파괴됬는지 확인 (true일 경우 포인터를 끊어낸도록 한다 => 끊어지다 보면 m_nReferenced가 0이 되어 메모리가 반환될 것이다. + true가 될때 내가 참조하고 있는 포인터들을 전부 끊는다.)
 
 	string m_name;	// 객체의 이름
-
-	bool m_destroyed;	// 해당 객체가 파괴됬는지 확인
 
 	XMFLOAT4X4 m_worldTransform;	// 모델->월드 좌표로 바꾸기 위한 행렬
 	XMFLOAT4X4 m_eachTransform;		// 부모 좌표 기준으로 자신을 변환하는 행렬
@@ -22,7 +21,9 @@ protected:
 	// 메테리얼 포인터들 초기화
 
 public:
+	// 생성자, 소멸자
 	GameObject();
+	~GameObject();
 
 };
 
